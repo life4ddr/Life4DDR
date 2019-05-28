d06b0528d20cc4754a2504aa74cc29f1ebb6c834
Corey Perrigo <corey@perrigogames.com>
2019-05-27 21:05:24 -0700

    allow notifications to be copied by tapping on them

--- app/src/main/java/com/perrigogames/life4trials/util/NotificationUtil.kt
+++ app/src/main/java/com/perrigogames/life4trials/util/NotificationUtil.kt
@@ -2,13 +2,27 @@ package com.perrigogames.life4trials.util
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.content.Context
+import android.app.PendingIntent
+import android.content.*
+import android.content.Context.CLIPBOARD_SERVICE
 import android.os.Build
+import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.perrigogames.life4trials.R
 import com.perrigogames.life4trials.activity.SettingsActivity
+import com.perrigogames.life4trials.util.NotificationUtil.EXTRA_COPY_VALUE
+
+class NotificationCopyHandler: BroadcastReceiver() {
+
+    override fun onReceive(c: Context, intent: Intent) {
+        (c.getSystemService(CLIPBOARD_SERVICE) as ClipboardManager).let {
+            it.primaryClip = ClipData.newPlainText("LIFE4 Data", intent.getStringExtra(EXTRA_COPY_VALUE))
+        }
+        Toast.makeText(c, c.getString(R.string.copied, intent.getStringExtra(EXTRA_COPY_VALUE)), Toast.LENGTH_SHORT).show()
+    }
+}
 
 object NotificationUtil {
 
@@ -17,8 +31,10 @@ object NotificationUtil {
 
     const val ID_NOTIF_RIVAL_CODE = 1001
     const val ID_NOTIF_TWITTER_HANDLE = 1002
-    const val ID_NOTIF_TRIAL_NAME = 1003
     const val ID_NOTIF_EX_SCORE = 1004
+
+    const val ACTION_COPY_CLIPBOARD = "ACTION_COPY_CLIPBOARD"
+    const val EXTRA_COPY_VALUE = "EXTRA_COPY_VALUE"
 
     private fun userInfoNotification(c: Context, @StringRes titleRes: Int, content: String) =
         NotificationCompat.Builder(c, ID_USER_INFO_CHANNEL)
@@ -44,18 +60,28 @@ object NotificationUtil {
     }
 
     fun showUserInfoNotifications(c: Context, exScore: Int) {
+        SharedPrefsUtils.getUserString(c, SettingsActivity.KEY_INFO_RIVAL_CODE)?.let { rivalCode ->
+            if (rivalCode.isNotEmpty()) {
+                notifyCopyableMessage(c, ID_NOTIF_RIVAL_CODE, R.string.rival_code, rivalCode)
+            }
+        }
+        SharedPrefsUtils.getUserString(c, SettingsActivity.KEY_INFO_TWITTER_NAME)?.let { twitterName ->
+            if (twitterName.isNotEmpty()) {
+                notifyCopyableMessage(c, ID_NOTIF_TWITTER_HANDLE, R.string.twitter_name, twitterName)
+            }
+        }
+        notifyCopyableMessage(c, ID_NOTIF_EX_SCORE, R.string.ex_score, exScore.toString())
+    }
+
+    private fun notifyCopyableMessage(c: Context, id: Int, @StringRes titleRes: Int, message: String) {
         with(NotificationManagerCompat.from(c)) {
-            SharedPrefsUtils.getUserString(c, SettingsActivity.KEY_INFO_RIVAL_CODE)?.let { rivalCode ->
-                if (rivalCode.isNotEmpty()) {
-                    notify(ID_NOTIF_RIVAL_CODE, userInfoNotification(c, R.string.rival_code, rivalCode).build())
-                }
-            }
-            SharedPrefsUtils.getUserString(c, SettingsActivity.KEY_INFO_TWITTER_NAME)?.let { twitterName ->
-                if (twitterName.isNotEmpty()) {
-                    notify(ID_NOTIF_TWITTER_HANDLE, userInfoNotification(c, R.string.twitter_name, twitterName).build())
-                }
-            }
-            notify(ID_NOTIF_EX_SCORE, userInfoNotification(c, R.string.ex_score, exScore.toString()).build())
+            notify(id, userInfoNotification(c, titleRes, c.getString(R.string.tap_to_copy, message))
+                .setContentIntent(PendingIntent.getBroadcast(c, id, Intent(c, NotificationCopyHandler::class.java).apply {
+                    putExtra(EXTRA_COPY_VALUE, message)
+                }, 0))
+                .setAutoCancel(true)
+                .build()
+            )
         }
     }
 }
