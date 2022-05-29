1844c7d4253df0d927a06ab840dfc38b201ba956
Corey Perrigo <corey@perrigogames.com>
2022-05-28 21:17:43 -0700

    GetScoreList error

--- app/src/main/java/com/perrigogames/life4/android/GetScoreList.kt
+++ app/src/main/java/com/perrigogames/life4/android/GetScoreList.kt
@@ -9,13 +9,14 @@ import androidx.activity.result.contract.ActivityResultContract
  * An [ActivityResultContract] that asks for a list of scores from another source, usually DDR A Score manager
  */
 class GetScoreList: ActivityResultContract<Unit, List<String>>() {
-    override fun createIntent(context: Context, u: Unit?) = Intent(ACTION_RETRIEVE_SCORE)
 
-    override fun parseResult(resultCode: Int, result: Intent?): List<String>? {
-        resultCode == Activity.RESULT_OK || return null
-        val count = result?.getIntExtra("SET_COUNT", 0) ?: 0
+    override fun createIntent(context: Context, input: Unit) = Intent(ACTION_RETRIEVE_SCORE)
+
+    override fun parseResult(resultCode: Int, intent: Intent?): List<String> {
+        resultCode == Activity.RESULT_OK || return emptyList()
+        val count = intent?.getIntExtra("SET_COUNT", 0) ?: 0
         return (0..count).flatMap {
-            result?.getStringExtra("SCORE_DATA_$it")?.split("\n")
+            intent?.getStringExtra("SCORE_DATA_$it")?.split("\n")
                 ?: emptyList()
         }.filterNot { it.isEmpty() }
     }
