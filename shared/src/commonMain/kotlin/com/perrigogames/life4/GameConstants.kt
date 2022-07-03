e414eeb05cf832fc560613ae6e5146a32dbfdebf
Corey Perrigo <corey@perrigogames.com>
2022-07-02 18:40:37 -0700

    MFC points tweak

--- shared/src/commonMain/kotlin/com/perrigogames/life4/GameConstants.kt
+++ shared/src/commonMain/kotlin/com/perrigogames/life4/GameConstants.kt
@@ -3,9 +3,14 @@ package com.perrigogames.life4
 object GameConstants {
 
     fun mfcPointsForDifficulty(difficulty: Int) = when(difficulty) {
-        8, 9, 10 -> 1
-        11, 12 -> 2
-        13 -> 4
+        1 -> 0.1f
+        2, 3 -> 0.25f
+        4, 5, 6 -> 0.5f
+        7, 8, 9 -> 1
+        10 -> 1.5f
+        11 -> 2
+        12 -> 4
+        13 -> 6
         14 -> 8
         15 -> 15
         16, 17, 18, 19, 20 -> 25
