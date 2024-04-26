2a17c54925e7680b80a28f928d1d611970d9bed2
Corey Perrigo <corey@perrigogames.com>
2024-04-26 00:04:15 -0700

    fix iOS

--- shared/src/iosMain/kotlin/com/perrigogames/life4/Platform.ios.kt
+++ shared/src/iosMain/kotlin/com/perrigogames/life4/Platform.ios.kt
@@ -5,18 +5,6 @@ import platform.Foundation.NSNumber
 import platform.Foundation.NSNumberFormatter
 import platform.Foundation.timeIntervalSince1970
 
-actual fun currentTimeMillis(): Long = (NSDate().timeIntervalSince1970 * 1000).toLong()
-
-internal actual fun printThrowable(t: Throwable) {
-    t.printStackTrace()
-}
-
-// FIXME
-internal actual fun logE(key: String, message: String) = Unit
-internal actual fun logException(t: Throwable) = Unit
-internal actual fun setCrashInt(key: String, v: Int) = Unit
-internal actual fun setCrashString(key: String, v: String) = Unit
-
 actual val isDebug = true // FIXME
 
 actual fun Int.longNumberString(): String {
