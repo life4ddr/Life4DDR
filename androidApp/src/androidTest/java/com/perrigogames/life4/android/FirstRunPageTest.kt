0314af80bf6d64a676f52af0c5bb9f39a7c5b07b
Scott Brenner <scott@scottbrenner.me>
2024-05-05 17:11:28 -0700

    Update FirstRunPageTest.kt

--- androidApp/src/androidTest/java/com/perrigogames/life4/android/FirstRunPageTest.kt
+++ androidApp/src/androidTest/java/com/perrigogames/life4/android/FirstRunPageTest.kt
@@ -1,22 +1,22 @@
 package com.perrigogames.life4.android
 
-//import androidx.test.espresso.Espresso.onView
-//import androidx.test.espresso.action.ViewActions.typeText
-//import androidx.test.espresso.assertion.ViewAssertions.matches
-//import androidx.test.espresso.matcher.ViewMatchers.*
-//import androidx.test.ext.junit.runners.AndroidJUnit4
-//import androidx.test.filters.LargeTest
-//import androidx.test.rule.ActivityTestRule
-//import com.perrigogames.life4.activity.firstrun.FirstRunInfoActivity
-//import com.perrigogames.life4.android.page.FirstRunPage
-//import com.perrigogames.life4.android.page.ReturningUserPopup
-//import org.junit.Rule
-//import org.junit.Test
-//import org.junit.runner.RunWith
+// import androidx.test.espresso.Espresso.onView
+// import androidx.test.espresso.action.ViewActions.typeText
+// import androidx.test.espresso.assertion.ViewAssertions.matches
+// import androidx.test.espresso.matcher.ViewMatchers.*
+// import androidx.test.ext.junit.runners.AndroidJUnit4
+// import androidx.test.filters.LargeTest
+// import androidx.test.rule.ActivityTestRule
+// import com.perrigogames.life4.activity.firstrun.FirstRunInfoActivity
+// import com.perrigogames.life4.android.page.FirstRunPage
+// import com.perrigogames.life4.android.page.ReturningUserPopup
+// import org.junit.Rule
+// import org.junit.Test
+// import org.junit.runner.RunWith
 //
-//@RunWith(AndroidJUnit4::class)
-//@LargeTest
-//class FirstRunPageTest {
+// @RunWith(AndroidJUnit4::class)
+// @LargeTest
+// class FirstRunPageTest {
 //
 //    private val page = FirstRunPage
 //    private val popup = ReturningUserPopup
@@ -45,4 +45,4 @@ package com.perrigogames.life4.android
 //        onView(popup.twitterLabel).check(matches(isDisplayed()))
 //        onView(popup.rankImage).check(matches(isDisplayed()))
 //    }
-//}
+// }
