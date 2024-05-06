c5aa22a9192c57b22af4c86955375a4bff2b66c5
Scott Brenner <scott@scottbrenner.me>
2024-05-05 17:10:41 -0700

    Update FirstRunPage.kt

--- androidApp/src/androidTest/java/com/perrigogames/life4/android/page/FirstRunPage.kt
+++ androidApp/src/androidTest/java/com/perrigogames/life4/android/page/FirstRunPage.kt
@@ -1,9 +1,9 @@
 package com.perrigogames.life4.android.page
 
-//import androidx.test.espresso.matcher.ViewMatchers.withId
-//import com.perrigogames.life4.android.R
+// import androidx.test.espresso.matcher.ViewMatchers.withId
+// import com.perrigogames.life4.android.R
 //
-//object FirstRunPage {
+// object FirstRunPage {
 //    val titleLabel = withId(R.id.text_title)
 //    val brandImage = withId(R.id.image_app_icon)
 //    val greetingsLabel = withId(R.id.text_greetings)
@@ -14,11 +14,11 @@ package com.perrigogames.life4.android.page
 //    val selectRankRadio = withId(R.id.radio_method_selection)
 //    val noRankRadio = withId(R.id.radio_method_no_rank)
 //    val signInButton = withId(R.id.button_continue)
-//}
+// }
 //
-//object ReturningUserPopup {
+// object ReturningUserPopup {
 //    val nameLabel = withId(R.id.text_player_name)
 //    val rivalLabel = withId(R.id.text_player_rival_code)
 //    val twitterLabel = withId(R.id.text_player_twitter)
 //    val rankImage = withId(R.id.image_rank)
-//}
+// }
