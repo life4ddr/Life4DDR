c3a982dd19b0594cc045972fc7ed8150ba1bc8b2
Corey Perrigo <corey@perrigogames.com>
2024-06-20 14:42:53 -0700

    fix PlacementListViewModel not correctly fetching initial data

--- shared/src/commonMain/kotlin/com/perrigogames/life4/feature/placements/PlacementListViewModel.kt
+++ shared/src/commonMain/kotlin/com/perrigogames/life4/feature/placements/PlacementListViewModel.kt
@@ -8,6 +8,8 @@ import dev.icerock.moko.mvvm.flow.cMutableStateFlow
 import dev.icerock.moko.mvvm.flow.cStateFlow
 import dev.icerock.moko.mvvm.viewmodel.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
@@ -18,6 +20,14 @@ class PlacementListViewModel : ViewModel(), KoinComponent {
 
     private val _screenData = MutableStateFlow(placementManager.createUiData()).cMutableStateFlow()
     val screenData: CStateFlow<UIPlacementListScreen> = _screenData.cStateFlow()
+
+    init {
+        viewModelScope.launch {
+            placementManager.placements
+                .map { placementManager.createUiData(it) }
+                .collect(_screenData)
+        }
+    }
 
     fun setFirstRunState(state: InitState) {
         firstRunSettingsManager.setInitState(state)
